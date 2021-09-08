38468aabee90110a2ff9122725227f94ea3a955d
PETER KIRSCHNER <peter@klib.io>
2021-09-08 08:50:08 +0200

    improve and align docker

--- buildAndPublish.sh
+++ buildAndPublish.sh
@@ -10,25 +10,40 @@ fi
 set -eauo pipefail
 
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
-DATE=$(date +'%Y.%m.%d-%H.%M.%S')
+DATE=$(date +'%Y-%m-%d_%H-%M-%S')
 IMAGE="klibio/osgi-starterkit"
 VERSION=`cat version.txt`
-echo "# launching docker build for image $IMAGE:$VERSION.$DATE"
+echo "# launching docker build for image $IMAGE"
 docker build \
   --no-cache \
   --progress=plain \
   --build-arg BUILD_DATE=$DATE \
   --build-arg VCS_REF=$(git rev-list -1 HEAD) \
   --build-arg VERSION=$VERSION \
-  -t "$IMAGE:$VERSION.$DATE" \
-  -t "$IMAGE:latest" \
-  .
+  -t $IMAGE \
+  . \
+
+# tagging and pushing docker image
 
 echo "$DOCKER_TOKEN" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push "$IMAGE:$VERSION.$DATE"
-echo "# successfully pushed $IMAGE:$VERSION.$DATE to DockerHub https://hub.docker.com/r/$IMAGE"
+
+# docker images are tagged with multiple tags depending on branch
+# all images are tagged with version and date
+# main branch will update the latest tag
+# feature/bugfix branches will get a tag with there branch name (replacing slash with dash)
+
+echo "# docker image tag $IMAGE $IMAGE:$VERSION.$DATE"
+docker image tag $IMAGE $IMAGE:$VERSION.$DATE
+
 if [ "$BRANCH" = "main" ]; then
-  docker push "$IMAGE:latest"
-  echo "# successfully updated $IMAGE:latest image on DockerHub https://hub.docker.com/r/$IMAGE"
+  echo "# docker image tag $IMAGE $IMAGE:latest"
+  docker image tag $IMAGE $IMAGE:latest
+else
+  echo "# docker image tag $IMAGE $IMAGE:${BRANCH/\//-}"
+  docker image tag $IMAGE $IMAGE:${BRANCH/\//-}
+
+  echo "# docker image tag $IMAGE $IMAGE:next"
+  docker image tag $IMAGE $IMAGE:next
 fi
+docker image push --all-tags $IMAGE
 exit 0
